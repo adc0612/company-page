d7ebd10de426dbe49b85967b0b0523c722a72d20
Dohu <adc0612@gmail.com>
2021-03-01 15:12:44 +0900

    css file separation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -30,49 +30,6 @@ export default {
 }
 </script>
 
-<style scoped>
-  
-  .week_title{
-    line-height: 50px;
-    text-align: center;
-    font-size: 1.5rem;
-    letter-spacing: 2px;
-    color: #0d3593;
-  }
-  .week_title>span{
-    font-size:0.9rem;
-    letter-spacing: 0;
-    color: #002377;
-  }
-  .menu_btn{
-    position: absolute;
-    font-size: 35px;
-    padding: 0 10px;
-    line-height: 50px;
-    color: rgb(117, 117, 117);
-    top: 0;
-    left: 0;
-    visibility: hidden;
-    opacity: 0;
-    transition: visibility 1s, opacity 0.5s linear;
-  }
-  .back_btn{
-    position: absolute;
-    font-size: 30px;
-    padding: 0 10px;
-    line-height: 50px;
-    color: rgb(117, 117, 117);
-    top: 0;
-    right: 5px;
-  }
-  .header_box::after {
-    content: "";
-    clear: both;
-  }
-  @media only screen and (max-width: 700px) {
-    .menu_btn{
-      opacity: 1;
-      visibility: visible;
-    }
-  }
+<style scoped src="../assets/styles/header.css">
+
 </style>
